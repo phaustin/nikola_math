fbf0f4f240c21c37001d5aeb2ea94786888c269c
Phil Austin <paustin@eos.ubc.ca>
2016-02-05 12:14:23 -0800

    add themes

--- test/conf.py
+++ test/conf.py
@@ -135,7 +135,10 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3"
+#THEME = "bootstrap3"
+THEME = "phil1"
+#THEME = "ipython"
+#THEME = "bootstrap3-jinja"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
---
+++ test/themes/phil1/README.md
@@ -0,0 +1,4 @@
+# tiao-nikola
+The tiao theme for Nikola
+https://github.com/ltiao/nikola-tiao
+
